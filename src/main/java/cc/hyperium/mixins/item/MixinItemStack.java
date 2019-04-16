f55f5d296d7d1b9e0c067d87a19844b2a5a48528
Reece Dunham <me@rdil.rocks>
2019-04-16 12:16:20 -0400

    cleanup

--- src/main/java/cc/hyperium/mixins/item/MixinItemStack.java
+++ src/main/java/cc/hyperium/mixins/item/MixinItemStack.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import java.util.List;
 
 @Mixin(ItemStack.class)
@@ -16,7 +15,6 @@ public class MixinItemStack {
     @Inject(method = "getTooltip", at = @At("RETURN"))
     private void injectTooltip(EntityPlayer playerIn, boolean advanced, CallbackInfoReturnable<List<String>> cir) {
         if (cir.getReturnValue() == null) return;
-
         new ItemTooltipEvent((ItemStack) (Object) this, cir.getReturnValue());
     }
 }
