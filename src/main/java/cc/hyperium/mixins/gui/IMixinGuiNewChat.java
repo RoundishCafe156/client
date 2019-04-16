90bcab530c241824ac123221f488d308653a76b5
Reece Dunham <me@rdil.rocks>
2019-04-16 12:21:03 -0400

    cleanup

--- src/main/java/cc/hyperium/mixins/gui/IMixinGuiNewChat.java
+++ src/main/java/cc/hyperium/mixins/gui/IMixinGuiNewChat.java
@@ -4,7 +4,6 @@ import net.minecraft.client.gui.ChatLine;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-
 import java.util.List;
 
 @Mixin(GuiNewChat.class)
