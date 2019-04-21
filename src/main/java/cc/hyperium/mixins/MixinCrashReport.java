2289c9c46ae796c339b0174782aa009d5c2c5b72
Reece Dunham <me@rdil.rocks>
2019-04-21 09:32:39 -0400

    Remove unused code in crash report mixin

--- src/main/java/cc/hyperium/mixins/MixinCrashReport.java
+++ src/main/java/cc/hyperium/mixins/MixinCrashReport.java
@@ -2,10 +2,7 @@ package cc.hyperium.mixins;
 
 import cc.hyperium.mixinsimp.HyperiumCrashReport;
 import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,13 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinCrashReport {
     private HyperiumCrashReport hyperiumCrashReport = new HyperiumCrashReport((CrashReport) (Object) this);
 
-    @Final
-    @Shadow
-    private CrashReportCategory theReportCategory;
-
     @Inject(method = "populateEnvironment", at = @At("HEAD"))
     public void add(CallbackInfo info) {
         hyperiumCrashReport.add(info);
     }
-
 }
