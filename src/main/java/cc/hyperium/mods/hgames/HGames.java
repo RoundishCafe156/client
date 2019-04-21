17bdc29b65ddfec5bf617a378161591fcc592d58
Reece Dunham <me@rdil.rocks>
2019-04-21 17:35:53 -0400

    Improve switch statement performance

--- src/main/java/cc/hyperium/mods/hgames/HGames.java
+++ src/main/java/cc/hyperium/mods/hgames/HGames.java
@@ -10,7 +10,6 @@ import cc.hyperium.mods.AbstractMod;
 import cc.hyperium.mods.chromahud.displayitems.hyperium.MinigameDisplay;
 import cc.hyperium.mods.hgames.minigames.Minigame;
 import cc.hyperium.mods.hgames.minigames.Walls3;
-import cc.hyperium.utils.ChatColor;
 
 public class HGames extends AbstractMod {
     private final Metadata metadata;
@@ -18,18 +17,13 @@ public class HGames extends AbstractMod {
 
     public HGames() {
         metadata = new Metadata(this, "Hypixel Minigames", "1.0", "Kevin");
-        metadata.setDisplayName(ChatColor.GOLD + "Hypixel Minigames");
     }
 
     @InvokeEvent
     private void onMinigameSwitch(JoinMinigameEvent event) {
-        switch (event.getMinigame()) {
-            case WALLS3:
-                minigame = new Walls3();
-                break;
-            default:
-                minigame = null;
-        }
+        if(event.getMinigame() == WALLS3) {
+            minigame = new Walls3();
+        } else minigame = null;
     }
 
     @InvokeEvent
