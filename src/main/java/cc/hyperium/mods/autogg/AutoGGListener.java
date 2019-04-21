68989e98e71adb744ba0efdd5d7518de215e65d5
Reece Dunham <me@rdil.rocks>
2019-04-21 18:01:49 -0400

    Performance improvement

--- src/main/java/cc/hyperium/mods/autogg/AutoGGListener.java
+++ src/main/java/cc/hyperium/mods/autogg/AutoGGListener.java
@@ -33,7 +33,7 @@ public class AutoGGListener {
         // Double parse to remove hypixel formatting codes
         String unformattedMessage = ChatColor.stripColor(event.getChat().getUnformattedText());
 
-        if (this.mod.getTriggers().stream().anyMatch(unformattedMessage::contains) && unformattedMessage.startsWith(" ")) {
+        if (this.mod.getTriggers().stream().anyMatch(unformattedMessage::contains) && unformattedMessage.charAt(0).equals(" ")) {
             this.mod.setRunning(true);
             invoked = true;
             Multithreading.runAsync(() -> {
