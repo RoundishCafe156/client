982eba4fb8063ff659cbb8486db96bfe3ce61cae
Reece Dunham <me@rdil.rocks>
2019-04-22 10:25:37 -0400

    Remove unused code

--- src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiChat.java
+++ src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiChat.java
@@ -2,15 +2,10 @@ package cc.hyperium.mixinsimp.gui;
 
 import cc.hyperium.Hyperium;
 import cc.hyperium.handlers.handlers.HypixelDetector;
-import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiTextField;
 
 public class HyperiumGuiChat {
-    private GuiChat parent;
-
-    public HyperiumGuiChat(GuiChat parent) {
-        this.parent = parent;
-    }
+    public HyperiumGuiChat() {}
 
     public void onSendAutocompleteRequest(String leftOfCursor) {
         Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().autoComplete(leftOfCursor);
