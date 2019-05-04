6a4f186dbd097ff2080f754e2e257700afea2fc1
Reece Dunham <me@rdil.rocks>
2019-05-04 17:22:31 -0400

    Cleanup

--- src/main/java/cc/hyperium/mods/timechanger/commands/CommandTimeChangerReset.java
+++ src/main/java/cc/hyperium/mods/timechanger/commands/CommandTimeChangerReset.java
@@ -21,10 +21,8 @@ import cc.hyperium.commands.BaseCommand;
 import cc.hyperium.handlers.handlers.chat.GeneralChatHandler;
 import cc.hyperium.mods.timechanger.TimeChanger;
 import cc.hyperium.mods.timechanger.TimeChanger.TimeType;
-import cc.hyperium.utils.ChatColor;
 
 public class CommandTimeChangerReset implements BaseCommand {
-
     private final TimeChanger mod;
 
     public CommandTimeChangerReset(TimeChanger main) {
@@ -38,12 +36,12 @@ public class CommandTimeChangerReset implements BaseCommand {
 
     @Override
     public String getUsage() {
-        return ChatColor.RED + "Usage: /resettime";
+        return "Usage: /resettime";
     }
 
     @Override
     public void onExecute(String[] args) {
         this.mod.setTimeType(TimeType.VANILLA);
-        GeneralChatHandler.instance().sendMessage(ChatColor.RED + "[TimeChanger] " + ChatColor.GREEN + "Now using vanilla time.", false);
+        GeneralChatHandler.instance().sendMessage("Now using vanilla time.", false);
     }
 }
