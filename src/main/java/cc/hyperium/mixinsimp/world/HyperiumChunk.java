7606d4ed3a4d8c3f14b1f3818edfd8af206dc22a
Reece Dunham <me@rdil.rocks>
2019-05-05 18:28:41 -0400

    Fix

--- src/main/java/cc/hyperium/mixinsimp/world/HyperiumChunk.java
+++ src/main/java/cc/hyperium/mixinsimp/world/HyperiumChunk.java
@@ -7,7 +7,6 @@ import net.minecraft.world.EnumSkyBlock;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class HyperiumChunk {
-
     public void getLightFor(EnumSkyBlock type, BlockPos pos, CallbackInfoReturnable<Integer> ci) {
         if (!Minecraft.getMinecraft().isIntegratedServerRunning() && Settings.FULLBRIGHT) {
             ci.setReturnValue(15);
