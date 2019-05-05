d95f0c3ff103dcd302f3f64dec521a1f3d27c7c3
Reece Dunham <me@rdil.rocks>
2019-05-05 18:26:11 -0400

    Cleanup mixin

--- src/main/java/cc/hyperium/mixinsimp/scoreboard/HyperiumScoreboard.java
+++ src/main/java/cc/hyperium/mixinsimp/scoreboard/HyperiumScoreboard.java
@@ -1,17 +1,10 @@
 package cc.hyperium.mixinsimp.scoreboard;
 
 import net.minecraft.scoreboard.ScorePlayerTeam;
-import net.minecraft.scoreboard.Scoreboard;
-
 import java.util.Map;
 
 public class HyperiumScoreboard {
-
-    private Scoreboard parent;
-
-    public HyperiumScoreboard(Scoreboard parent) {
-        this.parent = parent;
-    }
+    public HyperiumScoreboard() {}
 
     public void removeTeam(ScorePlayerTeam team, Map<String, ScorePlayerTeam> teams, Map<String, ScorePlayerTeam> teamMemberships) {
         if (team == null) {
@@ -21,8 +14,7 @@ public class HyperiumScoreboard {
         if (team.getRegisteredName() != null) {
             teams.remove(team.getRegisteredName());
         }
-
-        for (String s : team.getMembershipCollection()) {
+     for (String s : team.getMembershipCollection()) {
             teamMemberships.remove(s);
         }
 
