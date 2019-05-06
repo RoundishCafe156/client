8fb81efc77a7ea210e9412f6d33eca986bfcc64a
Reece Dunham <me@rdil.rocks>
2019-05-06 13:14:59 -0400

    cleanups

--- src/main/java/cc/hyperium/mods/oldanimations/OldAnimations.java
+++ src/main/java/cc/hyperium/mods/oldanimations/OldAnimations.java
@@ -2,17 +2,9 @@ package cc.hyperium.mods.oldanimations;
 
 import cc.hyperium.event.EventBus;
 import cc.hyperium.mods.AbstractMod;
-import cc.hyperium.utils.ChatColor;
 
 public class OldAnimations extends AbstractMod {
-
-    private final Metadata metadata;
-
-    public OldAnimations() {
-        Metadata data = new Metadata(this, "OldAnimations", "1.0", "Amplifiable");
-        data.setDisplayName(ChatColor.AQUA + "OldAnimations");
-        this.metadata = data;
-    }
+    public OldAnimations() {}
 
     @Override
     public AbstractMod init() {
@@ -22,6 +14,6 @@ public class OldAnimations extends AbstractMod {
 
     @Override
     public Metadata getModMetadata() {
-        return this.metadata;
+        return new Metadata(this, "OldAnimations", "1", "Amp");
     }
 }
