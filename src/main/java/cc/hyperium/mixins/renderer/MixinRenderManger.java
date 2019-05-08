bc61aaa89396e957544aed41bc2686ea2c483060
Reece Dunham <me@rdil.rocks>
2019-05-08 15:03:51 -0400

    Update MixinRenderManger.java

--- src/main/java/cc/hyperium/mixins/renderer/MixinRenderManger.java
+++ src/main/java/cc/hyperium/mixins/renderer/MixinRenderManger.java
@@ -13,6 +13,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.Map;
 
 @Mixin(RenderManager.class)
 public class MixinRenderManger implements IMixinRenderManager {
