08854b2454b69e0659fb1eb4d260117b30520961
Reece Dunham <me@rdil.rocks>
2019-05-08 18:16:59 -0400

    cleanup

--- src/main/java/cc/hyperium/utils/CrashHandler.java
+++ src/main/java/cc/hyperium/utils/CrashHandler.java
@@ -2,10 +2,8 @@ package cc.hyperium.utils;
 
 import cc.hyperium.installer.components.HScrollBarUI;
 import cc.hyperium.installer.components.VScrollBarUI;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
