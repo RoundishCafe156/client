1358f371b3c5af59ab05173ddfb87eaa4698d32f
Reece Dunham <me@rdil.rocks>
2019-05-09 18:37:34 -0400

    Update EventHandlerLite.java

--- src/main/java/cc/hyperium/mods/itemphysic/EventHandlerLite.java
+++ src/main/java/cc/hyperium/mods/itemphysic/EventHandlerLite.java
@@ -3,12 +3,12 @@ package cc.hyperium.mods.itemphysic;
 import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.event.RenderEvent;
 import cc.hyperium.mods.itemphysic.physics.ClientPhysic;
+import cc.hyperium.mixinsimp.entity.HyperiumEntityRenderer;
 
 public class EventHandlerLite {
-
     @InvokeEvent
     public void onTick(RenderEvent e) {
         ClientPhysic.tick = System.nanoTime();
+        HyperiumEntityRenderer.INSTANCE.disableBlurShader();
     }
-
 }
