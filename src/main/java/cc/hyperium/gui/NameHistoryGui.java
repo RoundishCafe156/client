5fcbcce11f22020993cebbd5a663682fdfdc1735
Reece Dunham <me@rdil.rocks>
2019-05-12 17:19:55 -0400

    fix

--- src/main/java/cc/hyperium/gui/NameHistoryGui.java
+++ src/main/java/cc/hyperium/gui/NameHistoryGui.java
@@ -30,7 +30,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import rocks.rdil.api.MojangAPI;
+import me.kbrewster.api.MojangAPI;
 
 public class NameHistoryGui extends GuiScreen {
     private String name;
