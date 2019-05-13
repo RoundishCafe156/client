7a9f651aa4c3ea682c4ce0967e74dd33b7ef7ba4
Reece Dunham <me@rdil.rocks>
2019-05-13 09:03:23 -0400

    Remove unused import

--- src/main/java/cc/hyperium/mixinsimp/world/HyperiumWorld.java
+++ src/main/java/cc/hyperium/mixinsimp/world/HyperiumWorld.java
@@ -22,7 +22,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class HyperiumWorld {
     private World parent;
