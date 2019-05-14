0b10a9425ecc1d9a72023ab24be9876415fa03f9
Reece Dunham <me@rdil.rocks>
2019-05-14 13:53:32 -0400

    Fixup

--- src/main/java/cc/hyperium/mods/itemphysic/ItemDummyContainer.java
+++ src/main/java/cc/hyperium/mods/itemphysic/ItemDummyContainer.java
@@ -1,14 +1,8 @@
 package cc.hyperium.mods.itemphysic;
 
 import cc.hyperium.config.ConfigOpt;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ItemDummyContainer {
-
-    public static final Logger logger = LogManager.getLogger("ItemPhysics");
-
     @ConfigOpt
     public static float rotateSpeed = 1.0F;
-
 }
