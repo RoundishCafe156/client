8c6dde23c2943bbf4b5ae1f06c9e8e85baf9945e
Reece Dunham <me@rdil.rocks>
2019-05-14 14:14:53 -0400

    fix

--- src/main/java/cc/hyperium/handlers/handlers/chat/PartyInviteChatHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/chat/PartyInviteChatHandler.java
@@ -3,11 +3,9 @@ package cc.hyperium.handlers.handlers.chat;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.HypixelPartyInviteEvent;
 import net.minecraft.util.IChatComponent;
-
 import java.util.regex.Matcher;
 
 public class PartyInviteChatHandler extends HyperiumChatHandler {
-
     @Override
     public boolean chatReceived(IChatComponent component, String text) {
         if (!text.toLowerCase().contains("their party!")) {
