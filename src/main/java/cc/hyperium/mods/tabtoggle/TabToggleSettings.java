d05016ff9e53d62f158c95f0d943ff4b4f558cbb
Reece Dunham <me@rdil.rocks>
2019-05-18 14:04:03 -0400

    Cleanup

--- src/main/java/cc/hyperium/mods/tabtoggle/TabToggleSettings.java
+++ src/main/java/cc/hyperium/mods/tabtoggle/TabToggleSettings.java
@@ -1,14 +1,12 @@
 package cc.hyperium.mods.tabtoggle;
 
-import cc.hyperium.config.Category;
+import static cc.hyperium.config.Category.TAB_TOGGLE;
 import cc.hyperium.config.ConfigOpt;
 import cc.hyperium.config.ToggleSetting;
 
 public class TabToggleSettings {
-
     public static boolean TAB_TOGGLED = false;
     @ConfigOpt
-    @ToggleSetting(name = "Tab Toggle", category = Category.TAB_TOGGLE, mods = true)
+    @ToggleSetting(name = "Tab Toggle", category = TAB_TOGGLE, mods = true)
     public static boolean ENABLED = false;
-
 }
