4bc2ac360339ae86a81286ded2e1cf437b111551
Reece Dunham <me@rdil.rocks>
2019-05-18 14:03:07 -0400

    Cleanup

--- src/main/java/cc/hyperium/mods/tabtoggle/TabToggleMod.java
+++ src/main/java/cc/hyperium/mods/tabtoggle/TabToggleMod.java
@@ -5,7 +5,6 @@ import cc.hyperium.event.EventBus;
 import cc.hyperium.mods.AbstractMod;
 
 public class TabToggleMod extends AbstractMod {
-
     @Override
     public AbstractMod init() {
         EventBus.INSTANCE.register(new TabToggleEventListener());
@@ -18,6 +17,6 @@ public class TabToggleMod extends AbstractMod {
 
     @Override
     public Metadata getModMetadata() {
-        return new Metadata(this, "Tab Toggle", "1.0", "KodingKing");
+        return new Metadata(this, "Tab Toggle", "1", "Hyperium");
     }
 }
