84a575dad27310f50400af65f907eec77ef5e4d1
Reece Dunham <me@rdil.rocks>
2019-05-21 13:46:32 -0400

    Try removing thing

--- src/main/java/cc/hyperium/cosmetics/DragonCosmetic.java
+++ src/main/java/cc/hyperium/cosmetics/DragonCosmetic.java
@@ -19,8 +19,6 @@ package cc.hyperium.cosmetics;
 
 import cc.hyperium.cosmetics.dragon.DragonHeadRenderer;
 import cc.hyperium.event.EventBus;
-import cc.hyperium.event.InvokeEvent;
-import cc.hyperium.event.RenderPlayerEvent;
 import cc.hyperium.purchases.EnumPurchaseType;
 import net.minecraft.client.entity.AbstractClientPlayer;
 
@@ -28,10 +26,5 @@ public class DragonCosmetic extends AbstractCosmetic {
     public DragonCosmetic() {
         super(false, EnumPurchaseType.DRAGON_HEAD);
         EventBus.INSTANCE.register(new DragonHeadRenderer(this));
-    }
-
-    @InvokeEvent
-    public void renderPlayer(RenderPlayerEvent event) {
-        AbstractClientPlayer player = event.getEntity();
     }
 }
