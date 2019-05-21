a39ee17761f8a90e1301a7b253fc6f02ade54643
Reece Dunham <me@rdil.rocks>
2019-05-21 11:31:14 -0400

    Add resource pack memory fix

---
+++ src/main/java/cc/hyperium/mods/memoryfix/TexturePackFix.java
@@ -0,0 +1,20 @@
+package cc.hyperium.mods.memoryfix;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ResourcePackImageScaler {
+    public static final int SIZE = 64;
+
+    public static BufferedImage scalePackImage(BufferedImage image) throws IOException {
+        if (image == null) {
+            return null;
+        }
+        BufferedImage smallImage = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+        Graphics graphics = smallImage.getGraphics();
+        graphics.drawImage(image, 0, 0, SIZE, SIZE, null);
+        graphics.dispose();
+        return smallImage;
+    }
+}
