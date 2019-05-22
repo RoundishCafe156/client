6dc70b7b14b35abb4256a02604a5c2d21df53235
Reece Dunham <me@rdil.rocks>
2019-05-22 13:46:02 -0400

    Merge together mixin

--- src/main/java/cc/hyperium/mixins/entity/MixinRenderFish.java
+++ src/main/java/cc/hyperium/mixins/entity/MixinRenderFish.java
@@ -1,8 +1,8 @@
 package cc.hyperium.mixins.entity;
 
-import cc.hyperium.mixinsimp.entity.HyperiumRenderFish;
 import net.minecraft.client.renderer.entity.RenderFish;
 import net.minecraft.entity.projectile.EntityFishHook;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,10 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderFish.class)
 public class MixinRenderFish {
-    private HyperiumRenderFish hyperiumRenderFish = new HyperiumRenderFish();
-
     @Inject(method = "doRender", at = @At("HEAD"))
     public void doRender(EntityFishHook entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
-        hyperiumRenderFish.doRender();
+        GL11.glLineWidth(1.0F); // Set line width to normal to prevent becoming thick
     }
 }
