89f28c96b94371e8393872172867b2e7581d0e2a
Reece Dunham <me@rdil.rocks>
2019-05-22 12:55:46 -0400

    lets work

--- src/main/java/me/semx11/autotip/universal/ReflectionUtil.java
+++ src/main/java/me/semx11/autotip/universal/ReflectionUtil.java
@@ -91,36 +91,26 @@ public class ReflectionUtil {
         UnableToFindMethodException(String[] methodNames, Exception e) {
             super("Could not find methods: " + String.join(", ", methodNames), e);
         }
-
     }
 
     public static class UnableToFindClassException extends RuntimeException {
-
         private static final long serialVersionUID = 3898634214210207487L;
-
         UnableToFindClassException(String[] classNames, Exception e) {
             super("Could not find classes: " + String.join(", ", classNames), e);
         }
-
     }
 
     public static class UnableToAccessFieldException extends RuntimeException {
-
         private static final long serialVersionUID = 3624431716334716913L;
-
         UnableToAccessFieldException(String[] fieldNames, Exception e) {
             super("Could not access fields: " + String.join(", ", fieldNames), e);
         }
-
     }
 
     public static class UnableToFindFieldException extends RuntimeException {
-
         private static final long serialVersionUID = 6746871885265039462L;
-
         UnableToFindFieldException(String[] fieldNames, Exception e) {
             super("Could not find fields: " + String.join(", ", fieldNames), e);
         }
-
     }
 }
