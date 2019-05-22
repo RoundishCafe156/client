e303554f6d04d6ae888c674f73d55d593b94301c
Reece Dunham <me@rdil.rocks>
2019-05-21 21:28:54 -0400

    Update MixinEntityRenderer.java

--- src/main/java/cc/hyperium/mixins/entity/MixinEntityRenderer.java
+++ src/main/java/cc/hyperium/mixins/entity/MixinEntityRenderer.java
@@ -83,77 +83,80 @@ public abstract class MixinEntityRenderer {
     @Overwrite
     public void getMouseOver(float partialTicks) {
         Entity entity = this.mc.getRenderViewEntity();
-        if (entity != null && this.mc.theWorld != null) {
-            this.mc.mcProfiler.startSection("pick");
-            this.mc.pointedEntity = null;
-            double d0 = (double) this.mc.playerController.getBlockReachDistance();
-            this.mc.objectMouseOver = entity.rayTrace(d0, partialTicks);
-            double d1 = d0;
-            Vec3 vec3 = entity.getPositionEyes(partialTicks);
-            boolean flag = false;
+        if (entity != null) {
+            if (this.mc.theWorld != null) {
+                this.mc.mcProfiler.startSection("pick");
+                this.mc.pointedEntity = null;
+                double d0 = (double) this.mc.playerController.getBlockReachDistance();
+                this.mc.objectMouseOver = entity.rayTrace(d0, partialTicks);
+                double d1 = d0;
+                Vec3 vec3 = entity.getPositionEyes(partialTicks);
+                boolean flag = false;
 
-            if (this.mc.playerController.extendedReach()) {
-                d0 = 6.0D;
-                d1 = 6.0D;
-            } else {
-                if (d0 > 3.0D) flag = true;
-            }
+                if (this.mc.playerController.extendedReach()) {
+                    d0 = 6.0D;
+                    d1 = 6.0D;
+                } else {
+                    if (d0 > 3.0D) flag = true;
+                }
 
-            if (this.mc.objectMouseOver != null) d1 = this.mc.objectMouseOver.hitVec.distanceTo(vec3);
+                if (this.mc.objectMouseOver != null) d1 = this.mc.objectMouseOver.hitVec.distanceTo(vec3);
 
-            Vec3 vec31 = entity.getLook(partialTicks);
-            Vec3 vec32 = vec3.addVector(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0);
-            this.pointedEntity = null;
-            Vec3 vec33 = null;
-            float f = 1.0F;
-            List<Entity> list = this.mc.theWorld.getEntitiesInAABBexcluding(entity, entity.getEntityBoundingBox().addCoord(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0).expand((double) f, (double) f, (double) f), Predicates.and(EntitySelectors.NOT_SPECTATING, Entity::canBeCollidedWith));
-            double d2 = d1;
+                Vec3 vec31 = entity.getLook(partialTicks);
+                Vec3 vec32 = vec3.addVector(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0);
+                this.pointedEntity = null;
+                Vec3 vec33 = null;
+                float f = 1.0F;
+                List<Entity> list = this.mc.theWorld.getEntitiesInAABBexcluding(entity, entity.getEntityBoundingBox().addCoord(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0).expand((double) f, (double) f, (double) f), Predicates.and(EntitySelectors.NOT_SPECTATING, Entity::canBeCollidedWith));
+                double d2 = d1;
 
-            for (int j = 0; j < list.size(); ++j) {
-                Entity entity1 = list.get(j);
-                float f1 = entity1.getCollisionBorderSize();
-                AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().expand((double) f1, (double) f1, (double) f1);
-                MovingObjectPosition movingobjectposition = axisalignedbb.calculateIntercept(vec3, vec32);
+                for (int j = 0; j < list.size(); ++j) {
+                    Entity entity1 = list.get(j);
+                    float f1 = entity1.getCollisionBorderSize();
+                    AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().expand((double) f1, (double) f1, (double) f1);
+                    MovingObjectPosition movingobjectposition = axisalignedbb.calculateIntercept(vec3, vec32);
 
-                if (axisalignedbb.isVecInside(vec3)) {
-                    if (d2 >= 0.0D) {
-                        this.pointedEntity = entity1;
-                        vec33 = movingobjectposition == null ? vec3 : movingobjectposition.hitVec;
-                        d2 = 0.0D;
-                    }
-                } else if (movingobjectposition != null) {
-                    double d3 = vec3.distanceTo(movingobjectposition.hitVec);
-                    if (d3 < d2 || d2 == 0.0D) {
-                        if (entity1 == entity.ridingEntity) {
-                            if (d2 == 0.0D) {
+                    if (axisalignedbb.isVecInside(vec3)) {
+                        if (d2 >= 0.0D) {
+                            this.pointedEntity = entity1;
+                            vec33 = movingobjectposition == null ? vec3 : movingobjectposition.hitVec;
+                            d2 = 0.0D;
+                        }
+                    } else if (movingobjectposition != null) {
+                        double d3 = vec3.distanceTo(movingobjectposition.hitVec);
+
+                        if (d3 < d2 || d2 == 0.0D) {
+                            if (entity1 == entity.ridingEntity) {
+                                if (d2 == 0.0D) {
+                                    this.pointedEntity = entity1;
+                                    vec33 = movingobjectposition.hitVec;
+                                }
+                            } else {
                                 this.pointedEntity = entity1;
                                 vec33 = movingobjectposition.hitVec;
+                                d2 = d3;
                             }
-                        } else {
-                            this.pointedEntity = entity1;
-                            vec33 = movingobjectposition.hitVec;
-                            d2 = d3;
                         }
                     }
                 }
-            }
-            double v = 0;
+                double v = 0;
 
-            if (this.pointedEntity != null && flag && (v = vec3.distanceTo(vec33)) > 3.0D) {
-                this.pointedEntity = null;
-                this.mc.objectMouseOver = new MovingObjectPosition(MovingObjectPosition.MovingObjectType.MISS, vec33, null, new BlockPos(vec33));
-            }
-            if (v != 0 || this.pointedEntity != null) ReachDisplay.dis = v;
-
-            if (this.pointedEntity != null && (d2 < d1 || this.mc.objectMouseOver == null)) {
-                this.mc.objectMouseOver = new MovingObjectPosition(this.pointedEntity, vec33);
-
-                if (this.pointedEntity instanceof EntityLivingBase || this.pointedEntity instanceof EntityItemFrame) {
-                    this.mc.pointedEntity = this.pointedEntity;
+                if (this.pointedEntity != null && flag && (v = vec3.distanceTo(vec33)) > 3.0D) {
+                    this.pointedEntity = null;
+                    this.mc.objectMouseOver = new MovingObjectPosition(MovingObjectPosition.MovingObjectType.MISS, vec33, null, new BlockPos(vec33));
                 }
-            }
+                if (v != 0 || this.pointedEntity != null) ReachDisplay.dis = v;
 
-            this.mc.mcProfiler.endSection();
+                if (this.pointedEntity != null && (d2 < d1 || this.mc.objectMouseOver == null)) {
+                    this.mc.objectMouseOver = new MovingObjectPosition(this.pointedEntity, vec33);
+
+                    if (this.pointedEntity instanceof EntityLivingBase || this.pointedEntity instanceof EntityItemFrame) {
+                        this.mc.pointedEntity = this.pointedEntity;
+                    }
+                }
+
+                this.mc.mcProfiler.endSection();
+            }
         }
     }
 }
