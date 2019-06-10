a71591fdcf2bf07d4e0548b7e7963b7a8e2797bc
Reece Dunham <me@rdil.rocks>
2019-06-10 14:38:21 -0700

    Ah

--- src/main/java/cc/hyperium/mixins/MixinPlayerControllerMP.java
+++ src/main/java/cc/hyperium/mixins/MixinPlayerControllerMP.java
@@ -10,7 +10,7 @@ import net.minecraft.init.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("unused")
 @Mixin(PlayerControllerMP.class)
