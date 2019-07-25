804a7d75d3866021b16d72514ad32761ffb0694c
Reece Dunham <me@rdil.rocks>
2019-07-25 12:50:54 -0400

    Update discord tag

--- src/main/java/rocks/rdil/jailbreak/Jailbreak.java
+++ src/main/java/rocks/rdil/jailbreak/Jailbreak.java
@@ -11,9 +11,8 @@ public class Jailbreak {
     public void debug() {
         Hyperium.LOGGER.info("----------------");
         Hyperium.LOGGER.warn("This project is NOT RUN BY THE HYPERIUM TEAM");
-        Hyperium.LOGGER.warn("Please report bugs by DMing jumbocakeyumyum#0001 on Discord");
+        Hyperium.LOGGER.warn("Please report bugs by DMing rdil#0001 on Discord");
         Hyperium.LOGGER.warn("or by emailing me@rdil.rocks");
-        Hyperium.LOGGER.warn("Thanks!");
         Hyperium.LOGGER.info("----------------");
     }
 
