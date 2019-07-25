d36364bd8de58b053e00f9cee79df1f5fe791b60
Reece Dunham <me@rdil.rocks>
2019-07-25 19:06:40 -0400

    k

--- src/main/java/cc/hyperium/mixins/renderer/MixinFontRenderer.java
+++ src/main/java/cc/hyperium/mixins/renderer/MixinFontRenderer.java
@@ -4,7 +4,6 @@ import cc.hyperium.handlers.HyperiumHandlers;
 import cc.hyperium.handlers.handlers.FontRendererData;
 import cc.hyperium.mixinsimp.client.GlStateModifier;
 import cc.hyperium.mixinsimp.renderer.CachedString;
-import cc.hyperium.mixinsimp.renderer.FontFixValues;
 import cc.hyperium.mixinsimp.renderer.StringHash;
 import cc.hyperium.mods.nickhider.NickHider;
 import net.minecraft.client.gui.FontRenderer;
