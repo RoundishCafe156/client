a8ef1a2ac669643681ad4bf1e08397a12f9c79d1
Reece Dunham <me@rdil.rocks>
2019-08-06 22:17:51 -0400

    Update MixinGuiControls.java

--- src/main/java/cc/hyperium/mixins/gui/MixinGuiControls.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiControls.java
@@ -1,25 +1,29 @@
 package cc.hyperium.mixins.gui;
 
-import cc.hyperium.mixinsimp.gui.HyperiumGuiControls;
+import cc.hyperium.gui.keybinds.GuiKeybinds;
+import cc.hyperium.mods.sk1ercommon.ResolutionUtil;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiControls;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.List;
 
 @Mixin(GuiControls.class)
 public class MixinGuiControls extends GuiScreen {
-    private HyperiumGuiControls hyperiumGuiControls = new HyperiumGuiControls();
-
     @Inject(method = "initGui", at = @At("RETURN"))
     private void initGui(CallbackInfo ci) {
-        hyperiumGuiControls.initGui(buttonList);
+        buttonList.add(new GuiButton(10, ResolutionUtil.current().getScaledWidth() / 2 - 60, ResolutionUtil.current().getScaledHeight() - 10, 120, 10, "Hyperium Binds"));
     }
 
     @Inject(method = "actionPerformed", at = @At("HEAD"))
     private void actionPerformed(GuiButton button, CallbackInfo ci) {
-        hyperiumGuiControls.actionPerformed(button);
+        if (button.id == 10) {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiKeybinds());
+        }
     }
 }
