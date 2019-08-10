97f380680d5c5f3597332f37e0f556c60f88cfff
Reece Dunham <me@rdil.rocks>
2019-08-09 21:37:22 -0400

    Fix

--- src/main/java/cc/hyperium/mixins/gui/MixinGuiButton.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiButton.java
@@ -29,25 +29,14 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(GuiButton.class)
 public abstract class MixinGuiButton extends Gui {
-    @Shadow
-    @Final
-    protected static ResourceLocation buttonTextures;
-    @Shadow
-    public boolean visible;
-    @Shadow
-    public int xPosition;
-    @Shadow
-    public int yPosition;
-    @Shadow
-    public String displayString;
-    @Shadow
-    protected boolean hovered;
-    @Shadow
-    protected int width;
-    @Shadow
-    protected int height;
-    private float selectPercent = 0.0f;
-    private long systemTime = Minecraft.getSystemTime();
+    @Shadow @Final protected static ResourceLocation buttonTextures;
+    @Shadow public boolean visible;
+    @Shadow public int xPosition;
+    @Shadow public int yPosition;
+    @Shadow public String displayString;
+    @Shadow protected boolean hovered;
+    @Shadow protected int width;
+    @Shadow protected int height;
     private HyperiumGuiButton hyperiumButton = new HyperiumGuiButton((GuiButton) (Object) this);
 
     @Overwrite
