cc526e33d8c3ef1aec1139f04c9ca47afd6aea1a
Reece Dunham <me@rdil.rocks>
2019-08-09 22:26:12 -0400

    Fix

--- src/main/java/cc/hyperium/mixinsimp/HyperiumCrashReport.java
+++ src/main/java/cc/hyperium/mixinsimp/HyperiumCrashReport.java
@@ -1,10 +1,10 @@
 package cc.hyperium.mixinsimp;
 
+import cc.hyperium.Hyperium;
 import cc.hyperium.commands.defaults.CommandDebug;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.CrashReportCategory;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import jb.Metadata;
 
 public class HyperiumCrashReport {
     private CrashReport parent;
@@ -15,7 +15,7 @@ public class HyperiumCrashReport {
 
     public void add(CallbackInfo info) {
         CrashReportCategory category = parent.makeCategoryDepth("Affected level", 1);
-        category.addCrashSection("Hyperium Version", Metadata.getVersion());
+        category.addCrashSection("Hyperium Version", Hyperium.modid);
         category.addCrashSection("Everything else", CommandDebug.get());
     }
 }
