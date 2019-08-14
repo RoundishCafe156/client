717744a3d62ccaeba07a4d2dfd82187b99da2502
Reece Dunham <me@rdil.rocks>
2019-08-14 19:01:41 -0400

    Kotlin conversion - about 30% done
    Signed-off-by: Reece Dunham <me@rdil.rocks>

---
+++ src/main/java/cc/hyperium/internal/addons/translate/MixinTranslator.java
@@ -0,0 +1,17 @@
+package cc.hyperium.internal.addons.translate;
+
+import cc.hyperium.internal.addons.AddonManifest;
+import java.util.List;
+import org.spongepowered.asm.mixin.Mixins;
+
+public final class MixinTranslator extends AbstractTranslator {
+    public void translate(AddonManifest manifest) {
+        List var10000 = manifest.getMixinConfigs();
+        if (var10000 != null) {
+            for (Object element$iv : var10000) {
+                String p1 = (String) element$iv;
+                Mixins.addConfiguration(p1);
+            }
+        }
+    }
+}
