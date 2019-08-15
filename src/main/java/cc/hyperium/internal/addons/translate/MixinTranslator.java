e840581a20dc607e734ce1ad9052395097d58f3f
Reece Dunham <me@rdil.rocks>
2019-08-15 11:10:03 -0400

    Fixed thing

--- src/main/java/cc/hyperium/internal/addons/translate/MixinTranslator.java
+++ src/main/java/cc/hyperium/internal/addons/translate/MixinTranslator.java
@@ -4,7 +4,7 @@ import cc.hyperium.internal.addons.AddonManifest;
 import java.util.List;
 import org.spongepowered.asm.mixin.Mixins;
 
-public final class MixinTranslator {
+public final class MixinTranslator implements ITranslator {
     public void translate(AddonManifest manifest) {
         List var10000 = manifest.getMixinConfigs();
         if (var10000 != null) {
