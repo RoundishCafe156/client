449be3873db422c8ff0a2aba71b6e32d17a3629d
Reece Dunham <me@rdil.rocks>
2019-08-24 23:00:25 -0400

    Try removing some icons

--- src/main/java/cc/hyperium/gui/Icons.java
+++ src/main/java/cc/hyperium/gui/Icons.java
@@ -6,11 +6,7 @@ import net.minecraft.util.ResourceLocation;
 public enum Icons {
     SETTINGS(new ResourceLocation("textures/material/settings.png")),
     EXTENSION(new ResourceLocation("textures/material/extension.png")),
-    EXIT(new ResourceLocation("textures/material/exit.png")),
-    ARROW_DOWN_ALT(new ResourceLocation("textures/material/arrow_down_alt.png")),
-    ARROW_UP_ALT(new ResourceLocation("textures/material/arrow_up_alt.png")),
-    ARROW_LEFT(new ResourceLocation("textures/material/arrow_left.png")),
-    ARROW_RIGHT(new ResourceLocation("textures/material/arrow_right.png"));
+    EXIT(new ResourceLocation("textures/material/exit.png"));
 
     private ResourceLocation res;
     Icons(ResourceLocation res) {
