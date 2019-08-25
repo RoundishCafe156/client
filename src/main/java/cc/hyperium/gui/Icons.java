4e75b9775706d46273995eebbcb478885897ec79
Reece Dunham <me@rdil.rocks>
2019-08-24 23:03:51 -0400

    Fix

--- src/main/java/cc/hyperium/gui/Icons.java
+++ src/main/java/cc/hyperium/gui/Icons.java
@@ -6,6 +6,8 @@ import net.minecraft.util.ResourceLocation;
 public enum Icons {
     SETTINGS(new ResourceLocation("textures/material/settings.png")),
     EXTENSION(new ResourceLocation("textures/material/extension.png")),
+    ARROW_DOWN_ALT(new ResourceLocation("textures/material/arrow_down_alt.png")),
+    ARROW_UP_ALT(new ResourceLocation("textures/material/arrow_up_alt.png")),
     EXIT(new ResourceLocation("textures/material/exit.png"));
 
     private ResourceLocation res;
