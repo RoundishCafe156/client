263ec9ab664e0d330e96dbf39ebb166b19987287
Reece Dunham <me@rdil.rocks>
2019-08-25 23:15:32 -0400

    Fix unused imports

--- src/main/java/cc/hyperium/mixins/IMixinMinecraft.java
+++ src/main/java/cc/hyperium/mixins/IMixinMinecraft.java
@@ -2,10 +2,8 @@ package cc.hyperium.mixins;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.util.Timer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Minecraft.class)
 public interface IMixinMinecraft {
