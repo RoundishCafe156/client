aaddb6b83071dbb2a5b472d02b590c7c483c7730
Reece Dunham <me@rdil.rocks>
2019-08-25 23:26:31 -0400

    bed time gn

--- src/main/java/cc/hyperium/mods/blockoverlay/BlockOverlay.java
+++ src/main/java/cc/hyperium/mods/blockoverlay/BlockOverlay.java
@@ -3,7 +3,6 @@ package cc.hyperium.mods.blockoverlay;
 import cc.hyperium.Hyperium;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.mods.AbstractMod;
-import cc.hyperium.utils.ChatColor;
 
 public class BlockOverlay extends AbstractMod {
     private BlockOverlaySettings settings;
